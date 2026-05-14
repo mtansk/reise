e8de2af2f0b7c918ae799d9291eb736a0db2af6b
mtansk <matwey2910@gmail.com>
2026-05-14 12:29:02 +0200

    chore: add and update dependencies, enable cache components

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,24 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
