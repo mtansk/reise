f347761605ee61b1f9e9cfc4f76b567a5687b7ca
mtansk <matwey2910@gmail.com>
2026-06-02 18:43:38 +0200

    chore: ensure errors are logged only in dev

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -2,10 +2,12 @@ import { createSafeActionClient } from "next-safe-action";
 
 export const actionClient = createSafeActionClient({
   handleServerError: (e) => {
-    console.error(e);
+    if (process.env.NODE_ENV === "development") {
+      console.error(e);
+    }
+
     return {
       message: e.message,
-      stack: e.stack,
     };
   },
 });
